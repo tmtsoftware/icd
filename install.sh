3339c6c9bf1a79aa2586bc96bbf6dfa9ea276463
Allan Brighton <Allan.Brighton@t-online.de>
2016-01-18 12:28:11 +0100

    Changed install script to avoid cp warnings on Linux

--- install.sh
+++ install.sh
@@ -6,7 +6,11 @@ dir=../install_icd
 
 test -d $dir || mkdir -p $dir/bin $dir/lib $dir/conf
 sbt stage "project root" stage
-for i in bin lib ; do cp -f */target/universal/stage/$i/* $dir/$i/; done
-for i in bin lib conf ; do cp -f icd-web/icd-web-server/target/universal/stage/$i/* $dir/$i/; done
-rm -f $dir/bin/*.log.* $dir/bin/*.bat
 
+for i in bin lib ; do
+    for j in */target/universal/stage/$i/* icd-web/icd-web-server/target/universal/stage/$i/* ; do
+        cp -f $j $dir/$i
+    done
+done
+
+rm -f $dir/bin/*.log.* $dir/bin/*.bat
