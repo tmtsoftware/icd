d281739146f340adccf451c7de6043ea7a61309b
weiss <weiss.personal@gmail.com>
2017-12-04 09:43:19 -0800

    moved listData logic to separate class

--- icd-db/src/main/scala/csw/services/icd/db/IcdDb.scala
+++ icd-db/src/main/scala/csw/services/icd/db/IcdDb.scala
@@ -6,7 +6,7 @@ import com.mongodb.casbah.Imports._
 import com.typesafe.config.{Config, ConfigFactory}
 import csw.services.icd._
 import csw.services.icd.model.{BaseModelParser, SubsystemModelParser}
-import icd.web.shared.IcdModels.TelemetryModel
+import csw.services.icd.db.ComponentDataReporter._
 import org.joda.time.{DateTime, DateTimeZone}
 
 import scala.io.StdIn
@@ -46,7 +46,7 @@ object IcdDb extends App {
 
     opt[String]("listData") valueName "<subsystem>" action { (x, c) =>
       c.copy(listData = Some(x))
-    } text "Specifies the subsystem of which the data rates are calculated."
+    } text "Prints a list of total data rates for components of specified subsystem."
 
     opt[String]('c', "component") valueName "<name>" action { (x, c) =>
       c.copy(component = Some(x))
@@ -127,7 +127,7 @@ object IcdDb extends App {
     options.diff.foreach(diffVersions)
     options.missing.foreach(missingItemsReport)
     options.archived.foreach(archivedItemsReport)
-    options.listData.foreach(listData)
+    options.listData.foreach(s => listData(db, s))
 
     // --list option
     def list(componentType: String): Unit = {
@@ -143,104 +143,6 @@ object IcdDb extends App {
     }
 
 
-    def listData(subsystem: String): Unit = {
-      val publishInfo = db.query.getPublishInfo(subsystem)
-      var componentTotalDataRate = 0.0
-      publishInfo.foreach { componentPublishInfo =>
-         println(s" ----  ${componentPublishInfo.componentName} ----- ")
-        componentTotalDataRate = 0.0
-        val componentModel = db.query.getComponentModel(subsystem, componentPublishInfo.componentName)
-        componentModel.foreach { cm =>
-          db.query.getPublishModel(cm).foreach { cpm =>
-            if (cpm.eventList.nonEmpty) {
-              println("--- Event Data")
-              componentTotalDataRate += listTelemetryData(cpm.eventList)
-            }
-            if (cpm.telemetryList.nonEmpty) {
-              println("--- Telemetry Data")
-              componentTotalDataRate += listTelemetryData(cpm.telemetryList)
-            }
-            if (cpm.eventStreamList.nonEmpty) {
-              println("--- EventSteam Data")
-              componentTotalDataRate += listTelemetryData(cpm.eventStreamList)
-            }
-          }
-        }
-        println(s"==== Total archived data rate for ${componentPublishInfo.componentName}: $componentTotalDataRate MB/hour")
-      }
-    }
-
-    def listTelemetryData(items: List[TelemetryModel]): Double = {
-      val DEFAULT_RATE = 0.1 // TODO move somewhere
-      var totalDataRate = 0.0
-      items.foreach { item =>
-        println(s"Item Name: ${item.name}, min rate = ${item.minRate}, max rate=${item.maxRate}, archiveRate=${item.archiveRate}, archive=${item.archive}")
-        if (item.archive) {
-          val rate = if (item.archiveRate > 0.0) {
-            item.archiveRate
-          } else if (item.maxRate > 0.0) {
-            item.maxRate
-          } else if (item.minRate > 0.0) {
-            item.minRate
-          } else {
-            DEFAULT_RATE
-          }
-          println(s"Item is archived at a rate of $rate Hz")
-          var itemData=8  // 8-bytes for timestamp
-          item.attributesList.foreach { att =>
-            print(s"-- Attribute ${att.name}: type=${att.typeStr}")
-            val eventSize = getSizeOfType(att.typeStr)
-            eventSize match {
-              case Some(s) =>
-                println(s", size=$s byte(s)")
-                itemData += s
-              case None => println(", cannot determine event size.  Skipping")
-            }
-          }
-          val dataRate = itemData * rate * 3600.0 / 1000000.0
-          if (itemData > 0) {
-            totalDataRate += dataRate
-            println(s"Total size of event: $itemData bytes.  data rate: $dataRate MB/hour")
-          }
-        }
-      }
-      println(s"Total data rate for this component model: $totalDataRate")
-      totalDataRate
-    }
-
-    def getSizeOfType(dtype: String): Option[Int] = dtype match {
-      case s if s.startsWith("boolean") => Some(1)
-      case s if s.startsWith("byte") => Some(1)
-      case s if s.startsWith("short") => Some(2)
-      case s if s.startsWith("enum") => Some(4)
-      case s if s.startsWith("integer") => Some(4)
-      case s if s.startsWith("float") => Some(4)
-      case s if s.startsWith("long") => Some(8)
-      case s if s.startsWith("double") => Some(8)
-      case s if s.startsWith("string") => Some(80)
-      case s if s.startsWith("array") =>
-        var s1 = s.drop(6)
-        var numElements = 1
-        var commaLoc = s1.indexOf(',')
-        val endLoc = s1.indexOf(']')
-        if (commaLoc < endLoc) {
-          while (commaLoc != -1) {
-            val s2 = s1.take(commaLoc)
-            numElements *= s2.toInt
-            s1 = s1.drop(commaLoc + 1)
-            commaLoc = s1.indexOf(',')
-          }
-        }
-        val newEndLoc = s1.indexOf(']')
-        val s3 = s1.substring(0, newEndLoc)
-        numElements *= s3.toInt
-        s1 = s1.drop(newEndLoc + 5)
-        getSizeOfType(s1) match {
-          case Some(i) => Some(i * numElements)
-          case None => None
-        }
-      case _ => None
-    }
 
     def error(msg: String): Unit = {
       println(msg)
