555d4aa7d2dcecfbca4a30758d66a859e4ee6ab3
abrighton <78px3WT5s>
2019-12-20 20:47:40 +0100

    Updated test

--- icd-db/src/test/scala/csw/services/icd/db/PerfTest.scala
+++ icd-db/src/test/scala/csw/services/icd/db/PerfTest.scala
@@ -7,18 +7,19 @@ import org.scalatest.Ignore
 @Ignore
 object PerfTest extends App {
   val db             = IcdDb("icds2")
+  val subsystem      = "TEST"
   val compName       = "Corrections"
-  val query          = IcdDbQuery(db.db, db.admin, Some(List("TEST")))
+  val query          = IcdDbQuery(db.db, db.admin, Some(List(subsystem)))
   val versionManager = IcdVersionManager(query)
 
   new ComponentInfoHelper(displayWarnings = false)
-    .getComponentInfo(versionManager, SubsystemWithVersion("TEST", None, Some(compName)))
+    .getComponentInfo(versionManager, SubsystemWithVersion(subsystem, None, Some(compName)))
     .foreach { info =>
       assert(info.componentModel.component == compName)
       assert(info.publishes.nonEmpty)
       assert(info.publishes.get.eventList.nonEmpty)
       info.publishes.get.eventList.foreach { pubInfo =>
-        println(s"lgsWfs publishes ${pubInfo.eventModel.name}")
+        println(s"$compName publishes ${pubInfo.eventModel.name}")
         pubInfo.subscribers.foreach { subInfo =>
           println(
             s"${subInfo.subscribeModelInfo.component} from ${subInfo.subscribeModelInfo.subsystem} subscribes to ${subInfo.subscribeModelInfo.name}"
