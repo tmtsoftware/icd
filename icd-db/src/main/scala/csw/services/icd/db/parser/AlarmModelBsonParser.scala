3aa3b8f80c84709ed0c96d9d080bdd1022761515
abrighton <78px3WT5s>
2020-01-03 16:29:14 +0100

    Removed test code

--- icd-db/src/main/scala/csw/services/icd/db/parser/AlarmModelBsonParser.scala
+++ icd-db/src/main/scala/csw/services/icd/db/parser/AlarmModelBsonParser.scala
@@ -13,17 +13,6 @@ object AlarmModelBsonParser {
 
   // Note: Some required args treated as optional for backward compatibility
   def apply(doc: BSONDocument): AlarmModel = {
-    val name = doc.getAsOpt[String]("name").get
-    val description = HtmlMarkup.gfmToHtml(doc.getAsOpt[String]("description").get)
-    val requirements = doc.getAsOpt[Array[String]]("requirements").map(_.toList).getOrElse(Nil)
-    val severityLevels = doc.getAsOpt[Array[String]]("severityLevels").map(_.toList).getOrElse(List("Warning", "Major", "Critical"))
-    val location = doc.getAsOpt[String]("location").getOrElse("")
-    val alarmType = doc.getAsOpt[String]("alarmType").getOrElse("")
-    val probableCause = HtmlMarkup.gfmToHtml(doc.getAsOpt[String]("probableCause").getOrElse(""))
-    val operatorResponse = HtmlMarkup.gfmToHtml(doc.getAsOpt[String]("operatorResponse").getOrElse(""))
-    val acknowledge = doc.getAsOpt[Boolean]("acknowledge").getOrElse(false)
-    val latched = doc.getAsOpt[Boolean]("latched").getOrElse(false)
-
     AlarmModel(
       name = doc.getAsOpt[String]("name").get,
       description = HtmlMarkup.gfmToHtml(doc.getAsOpt[String]("description").get),
