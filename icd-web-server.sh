93dcf90eae8d6bebc666a482bcb5f176f0c7b8e4
abrighto <Allan.Brighton@t-online.de>
2020-04-20 17:07:44 +0200

    Updated script for starting public server

--- icd-web-server.sh
+++ icd-web-server.sh
@@ -1,21 +1,8 @@
 #!/bin/sh
 
-# This script should be modified for use on a public server.
-# It can be used to run icdwebserver with publishing enabled, uploading disabled,
-# using https with a given certificate.
+# This script can be used to start the icd web app on a public server.
+# It can be used to run icdwebserver with publishing enabled and uploading disabled.
 
-# Edit these settings!
-#host=localhost
-#keystore=/shared/work/tmt/csw/icd/icd-web-server/conf/generated.keystore
-#secretKey='U3spB5^7L;EK_:<=OKxwpOJGM:0JsAyTu2pWhKsqbiGMO@tT_8voDNs:x7q=x?e<'
-
-exec icdwebserver \
-  -Dicd.allowUpload=false \
-  -Dhttp.port=80
-
-#  -Dhttp.port=disabled \
-#  -Dhttps.port=9443 \
-#  -Dhttps.address=$host \
-#  -Dplay.server.https.keyStore.path=$keystore \
-#  -Dplay.http.secret.key=$secretKey
-
+logdir=$HOME/.icd
+test -d $logdir || mkdir $logdir
+exec icdwebserver -Dicd.allowUpload=false -Dhttp.port=80 > $logdir 2>&1
