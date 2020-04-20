7ae31067c147541f4bddfb55da9540e9139f27e7
abrighto <Allan.Brighton@t-online.de>
2020-04-20 15:51:24 +0200

    Updated script for starting public server

--- icd-web-server.sh
+++ icd-web-server.sh
@@ -5,15 +5,17 @@
 # using https with a given certificate.
 
 # Edit these settings!
-host=localhost
-keystore=/shared/work/tmt/csw/icd/icd-web-server/conf/generated.keystore
-secretKey='U3spB5^7L;EK_:<=OKxwpOJGM:0JsAyTu2pWhKsqbiGMO@tT_8voDNs:x7q=x?e<'
+#host=localhost
+#keystore=/shared/work/tmt/csw/icd/icd-web-server/conf/generated.keystore
+#secretKey='U3spB5^7L;EK_:<=OKxwpOJGM:0JsAyTu2pWhKsqbiGMO@tT_8voDNs:x7q=x?e<'
 
 exec icdwebserver \
   -Dicd.allowUpload=false \
-  -Dhttp.port=disabled \
-  -Dhttps.port=9443 \
-  -Dhttps.address=$host \
-  -Dplay.server.https.keyStore.path=$keystore \
-  -Dplay.http.secret.key=$secretKey
+  -Dhttp.port=80
+
+#  -Dhttp.port=disabled \
+#  -Dhttps.port=9443 \
+#  -Dhttps.address=$host \
+#  -Dplay.server.https.keyStore.path=$keystore \
+#  -Dplay.http.secret.key=$secretKey
 
