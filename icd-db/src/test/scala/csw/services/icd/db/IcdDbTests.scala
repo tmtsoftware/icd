32b026c05b7175a8a84ff00d62825eada72c85a1
abrighto <Allan.Brighton@t-online.de>
2020-08-04 18:47:08 +0200

    Updated test

--- icd-db/src/test/scala/csw/services/icd/db/IcdDbTests.scala
+++ icd-db/src/test/scala/csw/services/icd/db/IcdDbTests.scala
@@ -68,87 +68,41 @@ class IcdDbTests extends AnyFunSuite {
     assert(!sensors.archive)
     val attrList = sensors.attributesList
 
-    val temp_ngsWfs = attrList.head
-    assert(temp_ngsWfs.name == "temp_ngsWfs")
+    val temp_ngsWfs = attrList.find(_.name == "temp_ngsWfs").get
     assert(temp_ngsWfs.description == "<p>NGS WFS temperature</p>")
     assert(temp_ngsWfs.typeStr == "float (-inf < x < inf, or NaN)")
+    assert(temp_ngsWfs.minimum.contains("-inf"))
+    assert(temp_ngsWfs.maximum.contains("inf"))
+    assert(temp_ngsWfs.allowNaN)
     assert(temp_ngsWfs.units == "<p>degC</p>")
+
+    val temp_lgsWfs = attrList.find(_.name == "temp_lgsWfs").get
+    assert(temp_lgsWfs.description == "<p>LGS WFS temperature</p>")
+    assert(temp_lgsWfs.typeStr == "float (0 < x < 100, or NaN)")
+    assert(temp_lgsWfs.minimum.contains("0"))
+    assert(temp_lgsWfs.maximum.contains("100"))
+    assert(temp_lgsWfs.allowNaN)
+    assert(temp_lgsWfs.units == "<p>degC</p>")
+
+    val temp_nacq = attrList.find(_.name == "temp_nacq").get
+    assert(temp_nacq.description == "<p>NACQ camera temperature</p>")
+    assert(temp_nacq.typeStr == "float")
+    assert(temp_nacq.minimum.isEmpty)
+    assert(temp_nacq.maximum.isEmpty)
+    assert(!temp_nacq.allowNaN)
+    assert(temp_nacq.units == "<p>degC</p>")
 
     // Test publish queries
     val published = db.query.getPublished(envCtrl, None).filter(p => p.name == "sensors" && p.publishType == CurrentStates)
     assert(published.size == 1)
     assert(published.head.publishType == CurrentStates)
 
-
-    //    val sensorList = db.query.publishes("test.ncc.env.ctrl.sensors", "TEST", Events)
-    //    assert(sensorList.size == 1)
-    //    assert(sensorList.head.componentName == "env.ctrl")
-    //    assert(sensorList.head.item.publishType == Events)
-    //    assert(sensorList.head.prefix == "test.ncc.env.ctrl")
-    //    assert(sensorList.head.item.name == "sensors")
-
     // Test accessing ICD models
     testModels(db)
-
-    //    // Test saving document from the database
-    //    IcdDbPrinter(db).saveToFile(envCtrl.subsystem, Some(envCtrl.component), new File("envCtrl.pdf"))
-    //    IcdDbPrinter(db).saveToFile("TEST", None, new File("TEST.pdf"))
-
-    // Test dropping a component
-//    db.query.dropComponent(envCtrl.subsystem, envCtrl.component)
-//    assert(db.query.getComponentModel("TEST", "envCtrl").isEmpty)
-
-    //    db.query.dropComponent("TEST")
-    //    assert(db.query.getComponentModel("TEST").isEmpty)
-    //    assert(db.query.getComponentModel("ndme").isEmpty)
 
     db.dropDatabase()
   }
 
-  // XXX TODO: Restore test after changes
-  //  test("Ingest and then update example ICD") {
-  //    val db = IcdDb("test")
-  //    db.dropDatabase() // start with a clean db for test
-  //
-  //    // These three different directories are ingested under the same name (example), to test versioning
-  //    testExample(db, "examples/example1", List("Test2"), "Comment for example1", majorVersion = false)
-  //    testExample(db, "examples/example2", List("TEST"), "Comment for example2", majorVersion = true)
-  //    testExample(db, "examples/example3", List("TEST"), "Comment for example3", majorVersion = false)
-  //
-  //    // Test Publish/Subscribe queries
-  //    val subscribeInfoList = db.query.subscribes("test2.parallacticAngle")
-  //    assert(subscribeInfoList.size == 1)
-  //    assert(subscribeInfoList.head.componentName == "TEST")
-  //    assert(subscribeInfoList.head.subscribeType == Events)
-  //    assert(subscribeInfoList.head.name == "test2.parallacticAngle")
-  //    assert(subscribeInfoList.head.subsystem == "TEST2")
-  //
-  //    val publishList = db.query.publishes("test.initialized")
-  //    assert(publishList.size == 1)
-  //    assert(publishList.head.componentName == "TEST")
-  //    assert(publishList.head.item.publishType == Events)
-  //    assert(publishList.head.prefix == "test")
-  //    assert(publishList.head.item.name == "initialized")
-  //
-  //    // Test versions
-  //    val versions = db.manager.getIcdVersions("example")
-  //    assert(versions.size == 3)
-  //    assert(versions.head.version == "2.1")
-  //    assert(versions.head.comment == "Comment for example3")
-  //    assert(versions(1).version == "2.0")
-  //    assert(versions(1).comment == "Comment for example2")
-  //    assert(versions(2).version == "1.0")
-  //    assert(versions(2).comment == "Comment for example1")
-  //
-  //    // Test diff
-  //    println("\nDiff example 2.0 2.1")
-  //    for (diff <- db.manager.diff("example", "2.0", "2.1")) {
-  //      // XXX TODO: add automatic test?
-  //      //      println(s"\n${diff.path}:\n${diff.patch.toString()}")
-  //    }
-  //
-  //    db.dropDatabase()
-  //  }
 
   // XXX TODO: Turn this into a test
   def testModels(db: IcdDb): Unit = {
@@ -178,21 +132,5 @@ class IcdDbTests extends AnyFunSuite {
       }
     }
   }
-
-  //  // Ingests the given dir under the name "example" (any previous version is saved in the history)
-  //  def testExample(db: IcdDb, path: String, componentNames: List[String], comment: String, majorVersion: Boolean): Unit = {
-  //    val problems = db.ingest(getTestDir(path), Some("example"), comment, majorVersion)
-  //    for (p <- problems) println(p)
-  //    assert(problems.isEmpty)
-  //
-  //    assert(db.query.getComponentNames == componentNames)
-  //
-  //    val components = db.query.getComponents
-  //    assert(components.size == componentNames.size)
-  //
-  //    // Test getting items based on the component name
-  //    val compModel = db.query.getComponentModel(componentNames.head).get
-  //    assert(compModel.name == componentNames.head)
-  //  }
 
 }
