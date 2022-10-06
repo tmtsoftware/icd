89921faddb8ed70c05fb7891ca7171dca511f0f9
Allan Brighton <Allan.Brighton@t-online.de>
2022-10-06 22:50:41 +0200

    Changed to display FITS tag in table when All is selected

--- icd-web-client/src/main/scala/icd/web/client/FitsKeywordDialog.scala
+++ icd-web-client/src/main/scala/icd/web/client/FitsKeywordDialog.scala
@@ -4,14 +4,43 @@ import icd.web.client.Components.ComponentListener
 import icd.web.shared.{FitsChannel, FitsKeyInfo, FitsSource, FitsTags}
 import org.scalajs.dom
 import org.scalajs.dom.{Element, HTMLInputElement, document}
-import org.scalajs.dom.html.Anchor
-import scalatags.JsDom
 import scalatags.JsDom.all._
 import org.scalajs.macrotaskexecutor.MacrotaskExecutor.Implicits._
 
-case class FitsKeywordDialog(fitsKeys: List[FitsKeyInfo], fitsTags: FitsTags, listener: ComponentListener) extends Displayable {
+object FitsKeywordDialog {
   private val allTags = "All"
-  
+
+  private val fitsTagNameCls = "fitsTagName"
+
+  // Inverts a map of T to List[T]
+  implicit class MapInverter[T](map: Map[T, List[T]]) {
+    def invert: Map[T, T] = {
+      val result = collection.mutable.Map.empty[T, T]
+
+      for ((key, values) <- map) {
+        for (v <- values) {
+          result += (v -> key)
+        }
+      }
+      result.toMap
+    }
+  }
+
+  private def hideElement(e: Element): Unit = {
+    e.classList.add("d-none")
+  }
+
+  private def showElement(e: Element): Unit = {
+    e.classList.remove("d-none")
+  }
+}
+
+case class FitsKeywordDialog(fitsKeys: List[FitsKeyInfo], fitsTags: FitsTags, listener: ComponentListener) extends Displayable {
+  import FitsKeywordDialog._
+
+  // Map from FITS keyword to list of tags for that keyword
+  private val tagMap = fitsTags.tags.invert
+
   // Action when user clicks on a component link
   private def clickedOnFitsSource(fitsSource: FitsSource)(e: dom.Event): Unit = {
     e.preventDefault()
@@ -22,45 +51,75 @@ case class FitsKeywordDialog(fitsKeys: List[FitsKeyInfo], fitsTags: FitsTags, li
   }
 
   // Makes the link for a FITS keyword source to the event that is the source of the keyword
-  private def makeLinkForFitsKeySource(fitsKey: FitsKeyInfo, fitsChannel: FitsChannel): JsDom.TypedTag[Anchor] = {
+  private def makeLinkForFitsKeySource(fitsKey: FitsKeyInfo, fitsChannel: FitsChannel, index: Int) = {
     import scalatags.JsDom.all._
-    a(
+    import scalacss.ScalatagsCss._
+
+    val maybeTag =
+      if (fitsChannel.name.isEmpty) {
+        tagMap.get(fitsKey.name)
+      }
+      else {
+        tagMap.get(s"${fitsKey.name}/${fitsChannel.name}")
+      }
+
+    div(
+      if (index != 0) hr else span(),
       id := s"${fitsKey.name}-${fitsChannel.name}-source",
-      title := s"Go to event parameter that is the source of this FITS keyword",
-      s"${fitsChannel.source.toLongString} ",
-      href := "#",
-      onclick := clickedOnFitsSource(fitsChannel.source) _
+      // Show the tag name
+      maybeTag.map(tag => span(cls := fitsTagNameCls, tag, ": ")),
+      a(
+        title := s"Go to event parameter that is the source of this FITS keyword",
+        s"${fitsChannel.source.toLongString} ",
+        href := "#",
+        onclick := clickedOnFitsSource(fitsChannel.source) _
+      )
     )
   }
 
+  //noinspection ScalaUnusedSymbol
   private def radioButtonListener(e: dom.Event): Unit = {
     val tag = document
       .querySelectorAll(s"input[name='fitsTag']:checked")
       .map(elem => elem.asInstanceOf[HTMLInputElement].value)
       .toList
       .head
+
+    // Only show tag names if All is selected
+    val tagNameElems = document.querySelectorAll(s".$fitsTagNameCls")
+    if (tag == allTags)
+      tagNameElems.toList.foreach(showElement)
+    else
+      tagNameElems.toList.foreach(hideElement)
+
     // Set which rows are visible based on the selected tag
     fitsKeys.foreach { fitsKey =>
       val elem = document.querySelector(s"#${fitsKey.name}")
-      if (tag == allTags)
-        elem.classList.remove("d-none")
+      if (tag == allTags) {
+        showElement(elem)
+        val channels = fitsKey.channels.map(_.name).filter(_.nonEmpty)
+        channels.foreach { c =>
+          val sourceElem = document.querySelector(s"#${fitsKey.name}-$c-source")
+          showElement(sourceElem)
+        }
+      }
       else {
         val tags = getTags(fitsKey)
         if (tags.contains(tag)) {
-          elem.classList.remove("d-none")
+          showElement(elem)
           // Set which source links are visible based on the channel
           val channels = fitsKey.channels.map(_.name).filter(_.nonEmpty)
           channels.foreach { c =>
             val showSource = fitsTags.tags(tag).contains(s"${fitsKey.name}/$c")
             val sourceElem = document.querySelector(s"#${fitsKey.name}-$c-source")
             if (showSource)
-              sourceElem.classList.remove("d-none")
+              showElement(sourceElem)
             else
-              sourceElem.classList.add("d-none")
+              hideElement(sourceElem)
           }
         }
         else
-          elem.classList.add("d-none")
+          hideElement(elem)
       }
     }
   }
@@ -106,6 +165,21 @@ case class FitsKeywordDialog(fitsKeys: List[FitsKeyInfo], fitsTags: FitsTags, li
   private def makeFitsKeyTable() = {
     import scalatags.JsDom.all._
     import scalacss.ScalatagsCss._
+
+    def makeFitsTableRows() = {
+      fitsKeys.map { fitsKey =>
+        val iList = fitsKey.channels.indices.toList
+        val zList = fitsKey.channels.zip(iList)
+        tr(id := fitsKey.name)(
+          td(fitsKey.name),
+          td(raw(fitsKey.description)),
+          td(fitsKey.typ),
+          td(fitsKey.units),
+          td(zList.map(p => makeLinkForFitsKeySource(fitsKey, p._1, p._2)))
+        )
+      }
+    }
+
     div(Styles.component, id := "FITS-Keys")(
       makeFitsTagPanel(),
       table(
@@ -121,16 +195,7 @@ case class FitsKeywordDialog(fitsKeys: List[FitsKeyInfo], fitsTags: FitsTags, li
           )
         ),
         tbody(
-          fitsKeys.map { fitsKey =>
-            // XXX TODO FIXME - add tag column if no tag selected (All tgs)
-            tr(id := fitsKey.name)(
-              td(fitsKey.name),
-              td(raw(fitsKey.description)),
-              td(fitsKey.typ),
-              td(fitsKey.units),
-              td(fitsKey.channels.map(c => makeLinkForFitsKeySource(fitsKey, c)))
-            )
-          }
+          makeFitsTableRows()
         )
       )
     )
