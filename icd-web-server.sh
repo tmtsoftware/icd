b984fefea23451a56d52ebb72fcc66695aa05db2
Allan Brighton <Allan.Brighton@t-online.de>
2025-08-16 22:21:15 +0200

    Added icd-git --ingestAll to start script for public icd web server

--- icd-web-server.sh
+++ icd-web-server.sh
@@ -6,5 +6,5 @@
 logdir=$HOME/.icd
 logfile=$logdir/icd.log
 test -d $logdir || mkdir $logdir
-exec ./icdwebserver -Dicd.isPublicServer=true -Dicd.pdf.cache.enabled=true -Dhttp.port=80 > $logfile 2>&1 &
-
+icd-git --ingestAll > $logfile 2>&1
+exec ./icdwebserver -Dicd.isPublicServer=true -Dicd.pdf.cache.enabled=true -Dhttp.port=80 >> $logfile 2>&1 &
