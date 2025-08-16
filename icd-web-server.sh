ab630e8e8d2ea6ed0b17f3c9db1216a0e48a40c6
Allan Brighton <Allan.Brighton@t-online.de>
2025-08-17 00:18:54 +0200

    Updating start script

--- icd-web-server.sh
+++ icd-web-server.sh
@@ -3,6 +3,7 @@
 # This script can be used to start the icd web app on a public server.
 # It can be used to run icdwebserver with publishing enabled and uploading disabled.
 
+set -x
 logdir=$HOME/.icd
 logfile=$logdir/icd.log
 test -d $logdir || mkdir $logdir
